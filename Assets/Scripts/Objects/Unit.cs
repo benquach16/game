b2bfe5b28adaa4a25451b6599d6763c125fd9000
U-Ben-PC\Ben <benquach16@yahoo.com>
2016-11-13 14:34:36 -0800

    fix tabs

--- Assets/Scripts/Objects/Unit.cs
+++ Assets/Scripts/Objects/Unit.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Unit : MonoBehaviour {
+public class Unit : Selectable
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    //replace with components?
+
+    enum E_ACTIONS
+    {
+        ACTION_STOP,
+        ACTION_MOVE,
+        ACTION_ATTACK,
+    }
+
+    E_ACTIONS m_currentCommand;
+
+    Vector3 m_position;
+    float m_rotation;
+
+    void Start ()
+    {
+        m_currentCommand = E_ACTIONS.ACTION_STOP;
+        m_position = new Vector3(0.0f, 0.0f, 0.0f);
+        m_rotation = 0.0f;
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+    
+    }
 }
