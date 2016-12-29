5d36270c8819377196d1a5a1a152431125faea14
Ben Quach <benquach16@yahoo.com>
2016-12-29 04:27:51 -0800

    get selection working

--- Assets/Scripts/Objects/Unit.cs
+++ Assets/Scripts/Objects/Unit.cs
@@ -8,10 +8,14 @@ public class Unit : Selectable
     // Use this for initialization
     public List<GameObject> m_components;
     bool selected = false;
-
+    Projector m_projSelection;
     void Start ()
     {
-
+        
+        GetComponent<Renderer>().material.color = new Color(0.5f, 0.5f, 1); //C#
+        m_projSelection = GetComponent<Projector>();
+        m_projSelection.enabled = false;
+        
     }
     
     // Update is called once per frame
@@ -20,14 +24,16 @@ public class Unit : Selectable
         //draw selection circle
         if(selected)
         {
-
+            
         }
     }
 
     private void OnMouseDown()
     {
         //do selection
+        Debug.Log("selected");
         selected = true;
+        m_projSelection.enabled = true;
     }
     void runAI()
     {
